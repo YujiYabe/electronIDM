dcda8d33e620213cb0f62bc36ea706346290f96c
Yuji Yabe <yayuji@gmail.com>
2019-10-23 20:53:20 +0900

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,42 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
+    <!-- <v-app-bar app>
+      <v-toolbar-title class='headline text-uppercase'>
         <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span class='font-weight-light'>MATERIAL DESIGN</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn text href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
-        <span class="mr-2">Latest Release</span>
+      <v-btn text href='https://github.com/vuetifyjs/vuetify/releases/latest' target='_blank'>
+        <span class='mr-2'>Latest Release</span>
       </v-btn>
-    </v-app-bar>
+    </v-app-bar>-->
 
     <v-content>
-      <HelloWorld />
+      <!-- <HelloWorld /> -->
+      <div id="flexbox_rght_pane" v-if="testvalue == null">
+        <RightFrame />
+      </div>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+// import HelloWorld from './components/HelloWorld'
+import RightFrame from './components/RightFrame'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    RightFrame
   },
+  computed: {
+    testvalue () {
+      console.log(this.$store.getters['testvalue'])
+      return this.$store.getters['testvalue']
+    }
+  },
+
   data: () => ({
     //
   })
--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,115 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    testvalue: 'gffdgdgdfss',
+    dataPosition: null,
+
+    dataSaveFile: {
+      boolVisibleIcon: false,
+      encryptKeyword: '',
+      dataDirectory: 'data',
+      historyList: null,
+      openFileType: 'normal',
+      saveFileType: 'normal',
+      optionSaveFileName: '',
+      selectHistoryFile: '',
+      selectHistoryIndex: null
+    },
+
+    dataDialog: {
+      save: false,
+      open: true
+    },
+
+    isDragged: false,
+
+    dataPrependIcon: {
+      id: false,
+      password: false,
+      other1: false,
+      other2: false
+    },
+
+    dataAppendIcon: {
+      all: false,
+      id: false,
+      password: false,
+      other1: false,
+      other2: false
+    },
+
+    dataFreeWord: '',
+
+    rules: {
+      required: value => !!value || 'Required.',
+      min: v => v.length >= 8 || 'Min 8 characters'
+    },
+    dataDictionary: {
+      placeholderBody: 'Please input',
+      placeholderOther: 'optional',
+      placeholderTag: 'changeable'
+    },
+
+    dataColorList: {
+      // itemList: "amber lighten-3",
+      // itemSelect: "pink lighten-3",
+      // name: "cyan lighten-4",
+
+      // id: "blue lighten-4",
+      // password: "green lighten-4",
+      // other1: "teal lighten-4",
+      // other2: "indigo lighten-4",
+      // text: "deep-purple lighten-4",
+
+      // tagId: "blue",
+      // tagPassword: "green",
+      // tagOther1: "teal",
+      // tagOther2: "indigo",
+      itemList: '',
+      itemSelect: '',
+      name: '',
+
+      id: '',
+      password: '',
+      other1: '',
+      other2: '',
+      text: '',
+
+      tagId: '',
+      tagPassword: '',
+      tagOther1: '',
+      tagOther2: ''
+    },
+
+    dataItemList: [],
+    dataSelectItem: {
+      index: null,
+      name: '',
+      id: '',
+      password: '',
+      other1: '',
+      other2: '',
+      text: ''
+    },
+
+    dataItemTemplate: {
+      name: '',
+      id: '',
+      password: '',
+      other1: '',
+      other2: '',
+      text: '',
+
+      tagId: 'ID',
+      tagPassword: 'password',
+      tagOther1: 'other1',
+      tagOther2: 'other2'
+    }
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
+  mutations: {},
+  actions: {},
+  modules: {},
+  getters: {
+    testvalue: state => state.testvalue
   }
 })
